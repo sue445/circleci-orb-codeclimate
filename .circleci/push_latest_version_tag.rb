ea6080854c0510b0df8286ef24393298c0247f90
sue445 <sue445@sue445.net>
2019-06-20 00:09:14 +0900

    Add release job

---
+++ .circleci/push_latest_version_tag.rb
@@ -0,0 +1,14 @@
+orb_ref = ARGV[0]
+
+raise "Usage: push_latest_version_tag.rb <namespace>/<orb-name>" unless orb_ref
+
+result = `circleci orb info #{orb_ref} --token #{ENV["CIRCLECI_API_TOKEN"]}`
+
+result =~ /^Latest:\s*#{orb_ref}@(.+)$/
+
+version = $1
+
+raise "Not found version in '#{result}'" unless version
+
+system "git tag -a #{version} -m 'Release #{version}'"
+system "git push origin --tags"
